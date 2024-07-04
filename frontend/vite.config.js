a01881295201f263c95690b34827983acd1d8f12
Felipe Valencia <r3id3n2091@hotmail.com>
2024-07-03 23:49:05 -0400

    actualizacion vite

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,11 +1,26 @@
-import { defineConfig } from 'vite';
+// import { defineConfig } from 'vite';
+// import react from '@vitejs/plugin-react';
+
+// export default defineConfig({
+//     plugins: [react()],
+//     server: {
+//         proxy: {
+//             '/api': 'http://localhost:8080',
+//         },
+//     },
+// });
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
-export default defineConfig({
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd()); // Cargar variables de entorno
+
+  return {
     plugins: [react()],
     server: {
-        proxy: {
-            '/api': 'http://localhost:8080',
-        },
+      proxy: env.NODE_ENV === 'production' ? {} : { // Proxy solo en desarrollo
+        '/api': 'http://localhost:8080',
+      },
     },
+  };
 });
